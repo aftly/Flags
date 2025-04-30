ec55fecd358010c63d7773ac492e79a516d97622
aftly <aftly@proton.me>
2025-04-30 01:23:37 +0100

    Update fullscreen state variables comment

--- app/src/main/java/dev/aftly/flags/ui/screen/flag/FlagScreen.kt
+++ app/src/main/java/dev/aftly/flags/ui/screen/flag/FlagScreen.kt
@@ -93,7 +93,7 @@ private fun FlagScaffold(
     description: List<String>,
     boldWordPositions: List<Int>,
 ) {
-    /* Managing state and screen orientation for full screen flag view */
+    /* Managing state and screen orientation for fullscreen flag view */
     var isFullScreen by rememberSaveable { mutableStateOf(value = false) }
     val orientationController = LocalOrientationController.current
 
