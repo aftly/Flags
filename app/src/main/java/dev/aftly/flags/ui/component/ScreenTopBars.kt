e226d40f303582498af36e18ee2e7f1163f08e9a
aftly <aftly@proton.me>
2025-05-07 22:49:14 +0100

    Fix animation of ExpandableTopAppBar title by using the weight() modifier on Box composables to control relative positioning

--- app/src/main/java/dev/aftly/flags/ui/component/ScreenTopBars.kt
+++ app/src/main/java/dev/aftly/flags/ui/component/ScreenTopBars.kt
@@ -1,10 +1,12 @@
 package dev.aftly.flags.ui.component
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -21,20 +23,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.lerp
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.util.lerp
 import dev.aftly.flags.navigation.Screen
 import dev.aftly.flags.ui.theme.Dimens
@@ -58,56 +54,43 @@ fun ExpandableTopAppBar(
         else -> currentScreen.title
     }
 
-    //var titleBoxWidth by remember { mutableIntStateOf(value = 0) }
-    //var titleTextWidth by remember { mutableIntStateOf(value = 0) }
-
-    /* Manage TitleStyle transition between expanded (start) and collapsed (stop) TopBar title */
+    /* Manage TitleStyle transition between expanded (start) and collapsed (stop) TopAppBar title */
     val titleStyle = lerp(
         start = MaterialTheme.typography.headlineLarge,
         stop = MaterialTheme.typography.headlineSmall,
         fraction = scrollBehaviour.state.collapsedFraction
     )
 
-    /* Manage padding transition between expanded (start) and collapsed (stop) TopBar title */
-    /*
-    val titleStartPadding = lerp(
-        start = 0.dp,
-        stop = Dimens.large24,
-        fraction = scrollBehaviour.state.collapsedFraction,
+    /* Manage transition of title from center to start position via box weight between
+     * expanded (start) and collapsed (stop) TopAppBar */
+    val boxWeight = lerp(
+        start = 1f,
+        stop = 0.001f,
+        fraction = scrollBehaviour.state.collapsedFraction
     )
-
-    val horizontalOffset = lerp(
-        start = 0,
-        stop = - (titleBoxWidth - titleTextWidth) / 2,
-        fraction = scrollBehaviour.state.collapsedFraction,
-    )
-     */
 
 
     MediumTopAppBar(
         title = {
             title?.let {
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(end = Dimens.medium16)
-                        /*
-                        .onSizeChanged { size ->
-                            titleBoxWidth = size.width
-                        }
-                         */
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
                 ) {
+                    Box(
+                        modifier = Modifier.size(0.dp)
+                            .weight(boxWeight)
+                    )
+
                     Text(
                         text = stringResource(it),
                         style = titleStyle,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.align(Alignment.Center)
-                            /*
-                            .onSizeChanged { size ->
-                                titleTextWidth = size.width
-                            }
-                             */
-                            //.offset { IntOffset(x = horizontalOffset, y = 0) }
-                            //.padding(start = titleStartPadding),
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Box(
+                        modifier = Modifier.size(0.dp)
+                            .weight(1f)
                     )
                 }
             }
