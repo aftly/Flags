35039c2dfe34b4f262fdc3bfc0060720571c282a
aftly <aftly@proton.me>
2025-05-22 00:25:12 +0100

    Update version info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.aftly.flags"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.7.8-alpha"
+        versionCode = 10
+        versionName = "0.8.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
