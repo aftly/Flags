03111b0c9ad72247791dfaae5dd2733a9cb21888
aftly <aftly@proton.me>
2025-06-12 03:15:42 +0100

    Start score history UI

--- app/src/main/java/dev/aftly/flags/ui/screen/gamehistory/GameHistoryScreen.kt
+++ app/src/main/java/dev/aftly/flags/ui/screen/gamehistory/GameHistoryScreen.kt
@@ -1,16 +1,25 @@
 package dev.aftly.flags.ui.screen.gamehistory
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Category
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.SportsScore
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,9 +30,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.aftly.flags.data.room.ScoreItem
@@ -31,7 +44,6 @@ import dev.aftly.flags.navigation.Screen
 import dev.aftly.flags.ui.component.ScoreDetails
 import dev.aftly.flags.ui.theme.Dimens
 import dev.aftly.flags.ui.util.formatTimestamp
-import dev.aftly.flags.ui.util.formatTimestampLegacy
 
 @Composable
 fun GameHistoryScreen(
@@ -132,10 +144,67 @@ private fun HistoryItem(
     item: ScoreItem,
     onScoreDetails: (ScoreItem) -> Unit,
 ) {
-    Row(modifier = modifier) {
-        Text(text = formatTimestamp(item.timestamp))
+    val padding = Dimens.extraSmall4
+    val dateTime = formatTimestamp(item.timestamp).replace(oldValue = " ", newValue = "\n")
+    val categoryIcon = Icons.Default.Category
+    val scoreIcon = Icons.Default.SportsScore
+    val timeModeIcon = Icons.Default.Timer
+    val dateTimeIcon = Icons.Default.DateRange
+    val container = MaterialTheme.colorScheme.primary
+    val content = MaterialTheme.colorScheme.onPrimary
+
+    Card(onClick = { onScoreDetails(item) }) {
+        Row(
+            modifier = modifier.padding(all = padding / 2),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            // TODO figure padding
+            /* Card {  } */
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = padding, vertical = padding / 2)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(container)
+            ) {
+                Icon(
+                    imageVector = dateTimeIcon,
+                    contentDescription = null,
+                    tint = content,
+                )
+                Text(
+                    text = dateTime,
+                    color = content,
+                    style = MaterialTheme.typography.labelMedium,
+                    lineHeight = MaterialTheme.typography.labelMedium.lineHeight * 0.75f
+                )
+            }
+        }
+    }
+    //Spacer(modifier = Modifier.height(padding))
+}
+
+
+/*
+@Composable
+private fun ItemElement(
+    icon: ImageVector,
+    text: String,
+    style: FontStyle,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+        )
+    ) {
+        Row {
+            Icon(imageVector = icon, contentDescription = null)
+            Text(text = text)
+        }
     }
 }
+ */
 
 
 @OptIn(ExperimentalMaterial3Api::class)
