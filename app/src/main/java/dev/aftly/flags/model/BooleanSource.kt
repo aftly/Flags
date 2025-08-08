14de1199e0473a5c6809d316eaad8bdad1cf1238
aftly <aftly@proton.me>
2025-08-08 02:52:22 +0100

    Update flag DataSource (missing flags and handling of <>Source types TODO)

--- app/src/main/java/dev/aftly/flags/model/BooleanSource.kt
+++ app/src/main/java/dev/aftly/flags/model/BooleanSource.kt
@@ -1,5 +1,6 @@
 package dev.aftly.flags.model
 
+/* For use in DataSource to handle compile time enforcement for downstream usage of the data */
 sealed interface BooleanSource {
     data class Explicit(val bool: Boolean) : BooleanSource
     object Inherit : BooleanSource
