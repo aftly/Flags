60d3f9d5152cb13b7a09a5c1c5e4d48c5da44ad4
aftly <aftly@proton.me>
2025-09-11 00:59:19 +0100

    Set general screen navigation animation timing to a quicker, navdrawer timing * 2 speed for more ui consistency

--- app/src/main/java/dev/aftly/flags/ui/theme/Timing.kt
+++ app/src/main/java/dev/aftly/flags/ui/theme/Timing.kt
@@ -2,9 +2,9 @@ package dev.aftly.flags.ui.theme
 
 /* Standardize animation timings */
 object Timing {
-    const val SCREEN_NAV: Int = 700
+    const val SCREEN_NAV: Int = 512
     const val DRAWER_NAV: Int = 256
-    const val MENU_EXPAND: Int = 500
+    const val MENU_EXPAND: Int = 512
     const val MENU_COLLAPSE: Int = 375
     const val GUESS_STATE_DURATION: Int = 1300
     const val GUESS_STATE_EXIT: Int = 700
