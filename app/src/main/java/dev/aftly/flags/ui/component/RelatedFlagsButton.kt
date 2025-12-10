d3ea997fc476873e0741d8228d8e9f49debb88a0
aftly <aftly@proton.me>
2025-12-10 03:41:08 +0000

    remove import

--- app/src/main/java/dev/aftly/flags/ui/component/RelatedFlagsButton.kt
+++ app/src/main/java/dev/aftly/flags/ui/component/RelatedFlagsButton.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import dev.aftly.flags.R
 import dev.aftly.flags.model.menu.FlagsMenu
